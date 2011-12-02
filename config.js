fb5caaca0ef7dec00e4a776f1552032cd2dfcad7
eugene <eugene@dell.(none)>
2011-12-02 14:06:04 +0200

    fixed incorrect config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.init = function() { 
     return {
         db : "xss",
-        root : "http://192.168.10.133:5984/xss/_design/api/_rewrite",
+        root : "http://127.0.0.1:5984/xss/_design/api/_rewrite",
         pageSize : 20
     };
 }
